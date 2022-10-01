12f09249198761eb3d9721ef1da3ad146c2349ed
kevinlinpt <kevinlinpt+1@hotmail.com>
2022-10-01 12:23:01 -0400

    inital commit

---
+++ src/Pages/HomePage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./HomePage.css"
+import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function HomePage() {
+  return (
+    <div>HomePage</div>
+  )
+}
+
+export default HomePage
